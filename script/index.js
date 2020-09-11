44928420823c992eb36d3b2e0bb0764ed78351af
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-09-11 09:23:41 -0500

    version 1.0

--- script/index.js
+++ script/index.js
@@ -6,19 +6,41 @@ const welcome = document.querySelector('.welcome')
 let name = document.querySelectorAll('.name')
 let mom = document.querySelector('.mom')
 let pet = document.querySelectorAll('.pet')
+const back = document.querySelector('.back')
+
 
 String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 start.addEventListener('click', () => {
-  welcome.style.display = 'none'
-  name.forEach(element => {
-    element.innerHTML = inputName.value.capitalize()
-  });
-  mom.innerHTML = inputMom.value.capitalize()
-  pet.forEach(element => {
-    element.innerHTML = inputPet.value.capitalize()
-  });
+  if (inputName.value == '' || inputMom.value == '') {
+    alert('Uppps! parece que olvidaste responder algunas preguntas')
+  } else if (inputPet.value == '') {
+    pet.forEach(element => {
+      element.innerHTML = 'Bobby'
+    });
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      document.body.scrollTop = 0; // For Safari
+      welcome.style.display = 'none'
+      name.forEach(element => {
+      element.innerHTML = inputName.value.capitalize()
+    });
+      mom.innerHTML = inputMom.value.capitalize()
+  } else {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    welcome.style.display = 'none'
+    name.forEach(element => {
+      element.innerHTML = inputName.value.capitalize()
+    });
+    mom.innerHTML = inputMom.value.capitalize()
+    pet.forEach(element => {
+      element.innerHTML = inputPet.value.capitalize()
+    });
+  }
 })
 
+back.addEventListener('click', () => {
+  document.location.reload()
+})
